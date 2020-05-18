448f69f60903d2f64b430f79327c2d2f10955694
ynosov <ynosov@determine.com>
2020-05-18 14:10:19 +0300

    Api Sourcing Event createion added

--- environment.js
+++ environment.js
@@ -1,10 +1,11 @@
 class Environment { 
     constructor() {
         //Evnironment variables
-        this.link = 'https://' + this.hostname + '/' + this.vrp + '/' + this.appli + '/';
-        this.appli = 'dsim_spa';
         this.host = 'autostandard72.determine.com';
-        this.vrp = 't'; // Version relative path
+        this.appli = 'dsim_spa';
+        this.vrp = 't';
+        this.url = 'https://' + hostname + '/' + vrp + '/' + appli + '/';
+        this.path = '/' + vrpt + '/' + appli;
         this.apitoken = '55118a102daf46ab8d9fc17a42515bfb';
 
         //Users
